2783cae7687725222bfa2600c43d0a331f1b891d
KietPham-VN <phamanhkiet.dev@gmail.com>
2025-05-16 16:49:28 +0700

    Add Book class and configuration for BookCategory
    - Introduced a new `Book` class in `Book.cs` with properties for `BookId`, `Title`, `Description`, `ISBN`, `Author`, and collections for `BookCategories` and `Reviews`.
    - Updated `BookCategoryConfiguration.cs` to implement `IEntityTypeConfiguration<BookCategory>`, defining the primary key and relationships between `BookCategory`, `Book`, and `Category`.

---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Commons.BaseEntities;
+
+namespace Domain.Entities;
+
+public class Book : AuditableEntity
+{
+    public Guid BookId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ISBN { get; set; } = string.Empty;
+    public string Author { get; set; } = string.Empty;
+
+    public ICollection<BookCategory> BookCategories { get; set; } = [];
+    public ICollection<Review> Reviews { get; set; } = [];
+}
---
+++ Infrastructure/Database/Configurations/BookCategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+public class BookCategoryConfiguration : IEntityTypeConfiguration<BookCategory>
+{
+    public void Configure(EntityTypeBuilder<BookCategory> builder)
+    {
+        builder.HasKey(bc => bc.BookCategoryId);
+
+        builder.HasOne(bc => bc.Book)
+               .WithMany(b => b.BookCategories)
+               .HasForeignKey(bc => bc.BookId);
+
+        builder.HasOne(bc => bc.Category)
+               .WithMany(c => c.BookCategories)
+               .HasForeignKey(bc => bc.CategoryId);
+    }
+}
